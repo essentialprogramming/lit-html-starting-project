4fb6121e227301b377d590daafb1577ab1f01097
iuliantacu-avangarde <75373520+iuliantacu-avangarde@users.noreply.github.com>
2021-03-02 15:17:43 +0200

    Add custom properties for background and outline color on the spinning cube.

--- components/spinning-cube/styles.js
+++ components/spinning-cube/styles.js
@@ -15,13 +15,16 @@ export const cubeStyles = css`
 
         :host {
         --face-size: 200px;
-          -webkit-box-flex: 1;
-          flex: 1 1 50%;
+        --base-color: #3e526a;
+        --outline-color: #02203c;
+
           display: -webkit-box;
           display: flex;
-          margin: 250px 0;
-          -webkit-box-pack: center;
+          flex: 1 1 50%;
           justify-content: center;
+          margin: 250px 0;
+          -webkit-box-flex: 1;
+          -webkit-box-pack: center;
         }
 
         @media (min-width: 992px) {
@@ -31,9 +34,9 @@ export const cubeStyles = css`
         }
 
         ::slotted(img) {
-          width: 100%;
           height: 100%;
           object-fit: contain;
+          width: 100%;
         }
 
         .cubeSpinnerContainer {
@@ -43,90 +46,83 @@ export const cubeStyles = css`
 
         .shadow-wrapper {
           position: relative;
-          width: var(--face-size);
           transform-style: preserve-3d;
+          width: var(--face-size);
         }
 
         .shadow {
-          position: absolute;
-          width: var(--face-size);
-          height: var(--face-size);
-          top: 0;
-          opacity: .98;
           box-shadow: 0 0 calc(var(--face-size) / 3.33) #000;
-          -webkit-transform: rotateX(90deg) translateY(calc(var(--face-size) / 3.33));
+          height: var(--face-size);
+          opacity: .98;
+          position: absolute;
+          top: 0;
           transform: rotateX(90deg) translateY(calc(var(--face-size) / 2));
-          -webkit-transform-origin: bottom center;
+          -webkit-transform: rotateX(90deg) translateY(calc(var(--face-size) / 2));
           transform-origin: bottom center;
+          -webkit-transform-origin: bottom center;
+          width: var(--face-size);
         }
 
         .cubeFaces, 
         .shadow-wrapper {
-          -webkit-animation: spin 45s linear infinite;
           animation: spin 45s linear infinite;
+          -webkit-animation: spin 45s linear infinite;
         }
 
         .cubeFaces {
-          position: relative;
-          width: var(--face-size);
           height: var(--face-size);
-          -webkit-transform-style: preserve-3d;
+          position: relative;
           transform-style: preserve-3d;
+          -webkit-transform-style: preserve-3d;
+          width: var(--face-size);
         }
 
         .cubeFace {
-          outline: 1px solid #02203c;
+          background-color: var(--base-color);
+          box-shadow: inset 0 0 calc(var(--face-size) / 3.33) #02203c;
+          height: 100%;
+          opacity: .75;
+          outline: 1px solid var(--outline-color);
           outline-offset: -1px;
           position: absolute;
           width: 100%;
-          height: 100%;
-          opacity: .75;
-          box-shadow: inset 0 0 calc(var(--face-size) / 3.33) #02203c;
         }
 
         .cubeFaceFront {
-          -webkit-transform: translateZ(calc(var(--face-size) / 2));
           transform: translateZ(calc(var(--face-size) / 2));
-          background-color: #3e526a;
+          -webkit-transform: translateZ(calc(var(--face-size) / 2));
         }
 
         .cubeFaceBack {
-          -webkit-transform: translateZ(calc(0px - var(--face-size) / 2)) rotateY(180deg);
           transform: translateZ(calc(0px - var(--face-size) / 2)) rotateY(180deg);
-          background-color: #3e526a;
-          background-color: var(--make);
-          
+          -webkit-transform: translateZ(calc(0px - var(--face-size) / 2)) rotateY(180deg);
         }
 
         .cubeFaceTop {
-          -webkit-transform: rotateX(-90deg) translateY(calc(0px - var(--face-size) / 2));
           transform: rotateX(-90deg) translateY(calc(0px - var(--face-size) / 2));
-          -webkit-transform-origin: top center;
+          -webkit-transform: rotateX(-90deg) translateY(calc(0px - var(--face-size) / 2));
           transform-origin: top center;
-          background-color: #02203c;
+          -webkit-transform-origin: top center;
         }
 
         .cubeFaceBottom {
-          -webkit-transform: rotateX(90deg) translateY(calc(var(--face-size) / 2));
           transform: rotateX(90deg) translateY(calc(var(--face-size) / 2));
-          -webkit-transform-origin: bottom center;
+          -webkit-transform: rotateX(90deg) translateY(calc(var(--face-size) / 2));
           transform-origin: bottom center;
-          background-color: #02203c;
+          -webkit-transform-origin: bottom center;
         }
 
         .cubeFaceLeft {
-          -webkit-transform: rotateY(270deg) translateX(calc(0px - var(--face-size) / 2));
           transform: rotateY(270deg) translateX(calc(0px - var(--face-size) / 2));
-          -webkit-transform-origin: center left;
+          -webkit-transform: rotateY(270deg) translateX(calc(0px - var(--face-size) / 2));
           transform-origin: center left;
-          background-color: #3e526a;
+          -webkit-transform-origin: center left;
         }
 
         .cubeFaceRight {
-          -webkit-transform: rotateY(-270deg) translateX(calc(var(--face-size) / 2));
           transform: rotateY(-270deg) translateX(calc(var(--face-size) / 2));
-          -webkit-transform-origin: top right;
+          -webkit-transform: rotateY(-270deg) translateX(calc(var(--face-size) / 2));
           transform-origin: top right;
-          background-color: #3e526a;
+          -webkit-transform-origin: top right;
         }
       `;
